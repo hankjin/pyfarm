c4033c19c007c2c83c141aafe4f98808af2aabf7
jindongh <jindongh@b1df7cea-a824-11de-a86f-b10fb6d3a35f>
2010-04-29 13:18:47 +0000

    添加micolog, 增加了memo功能

---
+++ trunk/other/helloworlp/cronMemo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#--encoding: utf-8 --
+import urllib,smtplib,base64,pickle,time,os
+import os,time,pickle
+from PyFetion import *
+
+def saveNotes(notes):
+    pickle.Pickler(open(noteFile,'w')).dump(notes)
+
+def inform(content):
+    print content
+    phone=PyFetion("13407100014","hjz851017","HTTP")
+    phone.login(FetionOnline)
+    phone.send_sms(content,"13407100014")
+if __name__=='__main__':
+    inform('hello')
+
